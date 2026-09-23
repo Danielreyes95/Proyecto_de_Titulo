041fe386cdf81023dfcec8f4550e110de883924c
Danielreyes95 <danielalejandroreyesparra@gmail.com>
2026-09-23 12:08:15 -0300

    feat: permitir invitaciones de director al superadmin

--- src/routes/platform.routes.js
+++ src/routes/platform.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { login, me } = require("../controllers/platform-auth.controller");
 const escuela = require("../controllers/platform-escuela.controller");
 const { requirePlatformAdmin } = require("../middleware/platform-auth");
+const { invitar } = require("../controllers/invitacion-director.controller");
 
 router.post("/auth/login", login);
 router.get("/auth/me", requirePlatformAdmin, me);
@@ -12,5 +13,6 @@ router.get("/escuelas", escuela.listar);
 router.post("/escuelas", escuela.crear);
 router.patch("/escuelas/:id", escuela.actualizar);
 router.patch("/escuelas/:id/branding", escuela.actualizarBranding);
+router.post("/escuelas/:escuelaId/invitar-director", invitar);
 
 module.exports = router;

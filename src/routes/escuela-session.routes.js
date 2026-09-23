1bee408ae7d7a74d303d3b21c24fe3a4ac5ddd06
Danielreyes95 <danielalejandroreyesparra@gmail.com>
2026-09-23 12:08:42 -0300

    feat: ruta marca editable solo por director autorizado

--- src/routes/escuela-session.routes.js
+++ src/routes/escuela-session.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const invite = require("../controllers/invitacion-director.controller");
 const session = require("../controllers/escuela-session.controller");
 const { requireSchoolUser, requireDirector } = require("../middleware/escuela-auth");
+const { actualizarBrandingDirector } = require("../controllers/escuela-branding.controller");
 
 router.post("/auth/login", session.login);
 router.get("/invitaciones/consultar", invite.consultar);
@@ -10,5 +11,6 @@ router.post("/invitaciones/aceptar", invite.aceptar);
 router.post("/invitaciones/aceptar-existente", requireSchoolUser, session.aceptarExistente);
 router.get("/mis-escuelas", requireSchoolUser, session.misEscuelas);
 router.get("/:escuelaId/director", requireSchoolUser, requireDirector, session.miEscuela);
+router.patch("/:escuelaId/branding", requireSchoolUser, requireDirector, actualizarBrandingDirector);
 
 module.exports = router;

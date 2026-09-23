9411999edd8d546ef00a8442fbdad8f680798357
Danielreyes95 <danielalejandroreyesparra@gmail.com>
2026-09-23 12:07:45 -0300

    feat: flujo onboarding director multiescuela (src/routes/escuela-session.routes.js)

---
+++ src/routes/escuela-session.routes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const invite = require("../controllers/invitacion-director.controller");
+const session = require("../controllers/escuela-session.controller");
+const { requireSchoolUser, requireDirector } = require("../middleware/escuela-auth");
+
+router.post("/auth/login", session.login);
+router.get("/invitaciones/consultar", invite.consultar);
+router.post("/invitaciones/aceptar", invite.aceptar);
+router.post("/invitaciones/aceptar-existente", requireSchoolUser, session.aceptarExistente);
+router.get("/mis-escuelas", requireSchoolUser, session.misEscuelas);
+router.get("/:escuelaId/director", requireSchoolUser, requireDirector, session.miEscuela);
+
+module.exports = router;
